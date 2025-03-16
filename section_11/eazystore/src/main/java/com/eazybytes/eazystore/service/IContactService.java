837effda2aa6c72d59a7fb1058a91c2d7e50d75e
eazybytes <tutor@eazybytes.com>
2025-03-16 18:18:45 +0530

    Thanks for choosing to learn from EazyBytes

--- section_11/eazystore/src/main/java/com/eazybytes/eazystore/service/IContactService.java
+++
@@ -1,7 +0,0 @@
-package com.eazybytes.eazystore.service;
-
-import com.eazybytes.eazystore.dto.ContactRequestDTO;
-
-public interface IContactService {
-    boolean saveContact(ContactRequestDTO contactRequest);
-}
